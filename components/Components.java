1c2d64c3584ff1ec2294c88502bb7836b28fab82
Ranielo <sultones5@gmail.com>
2023-12-08 19:46:03 +0800

    kani lang gamiton nato na component na file do

---
+++ components/Components.java
@@ -0,0 +1,13 @@
+package components;
+
+import javax.swing.*;
+
+public abstract class Components {
+    public static JFrame window = new JFrame();
+
+    public static JPanel loginAndRegisterPanel = new JPanel();
+
+    public static JPanel buttonPanel = new JPanel();
+    public static JPanel divPanel = new JPanel();
+
+}
