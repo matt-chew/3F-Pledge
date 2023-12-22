3806333ca0eddf2efd0355eb6beafa6a638634a5
Ranielo_Sultones <127005655+imjhirozen@users.noreply.github.com>
2023-12-22 14:57:36 +0800

    updated

--- components/Components.java
+++ components/Components.java
@@ -1,11 +1,20 @@
-package PledgeSystem.Components;
+package scholarship.components;
 
 import javax.swing.*;
 
 public abstract class Components {
     public static JFrame window = new JFrame();
-    public static JPanel infoPanel = new JPanel();
-    public static JPanel buttonPanel = new JPanel();
+
+    public static JPanel sideBarPanel = new JPanel();
     public static JPanel divPanel = new JPanel();
-    
+
+
+    public static ImageIcon loginAndRegisterImage = new ImageIcon("image/LoginAndRegisterBackgroundImage.jpg");
+
+    public static ImageIcon studentBtnImage = new ImageIcon("image/StudentBtn.png");
+    public static ImageIcon studentBtnImageClicked = new ImageIcon("image/StudentBtnClicked.png");
+
+    public static ImageIcon teacherBtnImage = new ImageIcon("image/TeacherBtn.png");
+    public static ImageIcon teacherBtnImageClicked = new ImageIcon("image/TeacherBtnClicked.png");
+
 }
