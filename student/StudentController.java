8d5574018609cd3c678a1d7569e98a0a50c5dea5
Ranielo_Sultones <127005655+imjhirozen@users.noreply.github.com>
2023-12-22 15:14:12 +0800

    new feature

---
+++ student/StudentController.java
@@ -0,0 +1,66 @@
+package scholarship.student;
+
+import scholarship.Database.DataController;
+import scholarship.components.Components;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class StudentController extends Components {
+
+    JPanel subPanel = new JPanel();
+
+    JLabel studentListLogo = new JLabel("LIST OF ALL STUDENT");
+
+    JTextArea studentList = new JTextArea();
+    JScrollPane scrollPane = new JScrollPane(studentList);
+
+    public StudentController (){
+
+        // main panel
+        StudentView.addPanel(window, divPanel, 157, 10, 925, 546, "#161A30");
+
+        // sub panel
+        StudentView.addLabel(subPanel, studentListLogo, 0, 0, 790, 60, 45, true, 1);
+        StudentView.addPanelToPanel(divPanel, subPanel, 60, 30, 800, 500, "#EEEDED");
+        displayStudents();
+
+    }
+
+    void jTextAreaDesign (){
+
+        studentList.setEditable(false);
+        studentList.setFont(new Font("Monospace", Font.PLAIN, 15));
+        studentList.setBackground(Color.decode("#67729D"));
+        studentList.setForeground(Color.WHITE);
+
+    }
+
+    void displayStudents (){
+
+        StudentView.addJScrollPane(subPanel, scrollPane, 20, 60, 760, 430);
+
+        jTextAreaDesign();
+
+        java.util.List<java.util.List<String>> data = DataController.listOfApplicants( "scholars" );
+
+        studentList.setText(" ID:" + "\t" + " NAME:" + "\t" + " LASTNAME:" + "\t" + " SECTION:" + "\t" + " BATCH:" + "\t" + " SCHOLARSHIP:" + "\n");
+        for(List<String> row : data){
+
+            studentList.append(" " + row.get(0) + "\t" + row.get(1) + "\t" + row.get(2) + "\t" + row.get(3) + "\t" + row.get(4) + "\t" + row.get(5) + "\n");
+
+        }
+
+        java.util.List<java.util.List<String>> data2 = DataController.listOfApplicants( "applicants" );
+
+        for(List<String> rows : data2){
+
+            studentList.append(" " + rows.get(0) + "\t " + rows.get(1) + "\t " + rows.get(2) + "\t " + rows.get(3) + "\t " + rows.get(4) + "\t " + rows.get(5) + "\n");
+
+        }
+
+    }
+
+}
